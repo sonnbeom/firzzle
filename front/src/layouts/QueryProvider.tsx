1cf6a74659949017aa79348661055d96da30ab47
antis <rkdwldms42@naver.com>
2025-04-14 17:51:23 +0900

    chore: 프론트엔드 초기세팅

---
+++ front/src/layouts/QueryProvider.tsx
@@ -0,0 +1,17 @@
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactNode } from 'react';
+import { queryClient } from 'utils/queryClient';
+
+interface QueryProviderProps {
+  children: ReactNode;
+}
+
+export const QueryProvider = ({ children }: QueryProviderProps) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools buttonPosition={'bottom-left'} />
+    </QueryClientProvider>
+  );
+};
