ada218309fd7fa9dfddee96c88dbafef83de695c
손승범 <no1beom@naver.com>
2025-04-22 13:05:46 +0900

    [BE] init: 게이트웨이 버젼 별로 라우팅 설정

--- back/firzzle/learning/src/main/java/com/firzzle/learning/test/LearningTestControllerV2.java
+++ back/firzzle/learning/src/main/java/com/firzzle/learning/test/LearningTestControllerV2.java
@@ -1,16 +1,18 @@
-package com.firzzle.auth.test;
+package com.firzzle.learning.test;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Log4j2
 @RestController
 @RequiredArgsConstructor
-public class TestController {
+@RequestMapping("/v2")
+public class LearningTestControllerV2 {
     @GetMapping("/test")
     public String test(){
-        return "테스트 호출 - 인증 서버";
+        return "테스트 호출 - 학습 v2 서버";
     }
 }
