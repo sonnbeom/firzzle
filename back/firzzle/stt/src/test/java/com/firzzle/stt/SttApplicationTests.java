e3cbc0f8950de03009df8e2a26d080613ddc701b
문종하 <3477qq@naver.com>
2025-04-23 17:43:12 +0900

    init: 기본 모듈 추가

---
+++ back/firzzle/stt/src/test/java/com/firzzle/stt/SttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firzzle.stt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SttApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
