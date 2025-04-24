09aa9562a663bb0ced3839acc6b1a5233b0eaa89
antis <rkdwldms42@naver.com>
2025-04-24 12:47:45 +0900

    fix: 빌드 오류 해결 1. queryClient를 별도로 분리하지 않고 QueryProvider에서 정의 2. global.css에서 잘못 지정된 스타일명 수정

--- front/.next/static/chunks/webpack.js
+++ front/.next/static/chunks/webpack.js
@@ -89,6 +89,18 @@
 /******/ 		};
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/create fake namespace object */
 /******/ 	(() => {
 /******/ 		var getProto = Object.getPrototypeOf ? (obj) => (Object.getPrototypeOf(obj)) : (obj) => (obj.__proto__);
@@ -178,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("a0da198950cc258b")
+/******/ 		__webpack_require__.h = () => ("d4742b8587ad8990")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
