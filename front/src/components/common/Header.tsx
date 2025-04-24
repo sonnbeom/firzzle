844bd263328fd0b8fb22782cd0c012359e4bdff4
antis <rkdwldms42@naver.com>
2025-04-24 10:44:19 +0900

    chore: 프론트엔드 페이지 라우터 및 tailwind css 설정

---
+++ front/src/components/common/Header.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+
+const Header = () => {
+  return (
+    <div className='w-full'>
+      <Image src='/assets/images/Firzzle.png' alt='logo' />
+    </div>
+  );
+};
+
+export default Header;
