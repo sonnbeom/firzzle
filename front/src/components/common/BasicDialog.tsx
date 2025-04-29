800b8a022e1dffacab590331b45c88b222280435
안정길 <jeonggilahn@gmail.com>
2025-04-29 14:40:06 +0900

    feat : Dialog 추가 및 BasicDialog 생성

---
+++ front/src/components/common/BasicDialog.tsx
@@ -0,0 +1,40 @@
+import { ReactNode } from 'react';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+interface BasicDialogProps {
+  isOpen: boolean;
+  onOpenChange: (open: boolean) => void;
+  title: string;
+  description: string;
+  children: React.ReactNode;
+}
+
+const BasicDialog = ({
+  isOpen,
+  onOpenChange,
+  title,
+  description,
+  children,
+}: BasicDialogProps) => {
+
+  return (
+    <Dialog open={isOpen} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+        <div className='mt-4 flex justify-end space-x-4'>
+          {children}
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default BasicDialog;
