803d75f78662af81281ab39073f65f7d42cdc0e7
antis <rkdwldms42@naver.com>
2025-04-30 15:56:32 +0900

    fix: 빌드 에러 해결

--- front/vitest.config.ts
+++ front/vitest.config.ts
@@ -1,9 +1,6 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-
-import { storybookTest } from '@storybook/experimental-addon-test/vitest-plugin';
 import { defineConfig } from 'vitest/config';
-
 
 const dirname =
   typeof __dirname !== 'undefined'
@@ -16,11 +13,6 @@ export default defineConfig({
     workspace: [
       {
         extends: true,
-        plugins: [
-          // The plugin will run tests for the stories defined in your Storybook config
-          // See options at: https://storybook.js.org/docs/writing-tests/test-addon#storybooktest
-          storybookTest({ configDir: path.join(dirname, '.storybook') }),
-        ],
         test: {
           name: 'storybook',
           browser: {
