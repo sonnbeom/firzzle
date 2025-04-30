ed14b35a11fb1deca82ca7612aaa0d950a5e2235
손승범 <no1beom@naver.com>
2025-04-30 12:47:44 +0900

    [BE] Feat: logging 모듈 구체화

---
+++ back/firzzle/user-domain/src/test/java/com/firzzle/userdomain/UserDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firzzle.userdomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
