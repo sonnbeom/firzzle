046dd256cfce8757658f4ef98c32359c25c4cd88
antis <rkdwldms42@naver.com>
2025-04-30 15:57:23 +0900

    fix: 불필요한 코드 삭제

--- front/vitest.config.ts
+++ front/vitest.config.ts
@@ -1,13 +1,5 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vitest/config';
 
-const dirname =
-  typeof __dirname !== 'undefined'
-    ? __dirname
-    : path.dirname(fileURLToPath(import.meta.url));
-
-// More info at: https://storybook.js.org/docs/writing-tests/test-addon
 export default defineConfig({
   test: {
     workspace: [
