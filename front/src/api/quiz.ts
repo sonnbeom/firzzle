0177007ad986653f2d1c4409f5d02f8cf1d8ad78
안정길 <jeonggilahn@gmail.com>
2025-05-02 14:31:57 +0900

    fix : quiz.ts 형식 변경

--- front/src/api/quiz.ts
+++ front/src/api/quiz.ts
@@ -1,12 +1,20 @@
-import { api } from './common/apiInstance';
+import { ApiResponseWithData } from '@/types/common/apiResponse';
 import { QuizData, QuizSubmitRequest, QuizSubmitResponse } from '@/types/quiz';
+import { externalApi } from './common/apiInstance';
 
 // 퀴즈 조회
 export const getQuiz = (contentId: string) => {
-  return api.get<QuizData[]>(`/contents/${contentId}/quiz`);
+  return externalApi.get(`/contents/${contentId}/quiz`) as Promise<
+    ApiResponseWithData<QuizData[]>
+  >;
 };
 
 // 퀴즈 정답 제출
-export const submitQuizAnswers = (contentId: string, request: QuizSubmitRequest) => {
-  return api.post<QuizSubmitResponse, QuizSubmitRequest>(`/contents/${contentId}/quiz/submit`, { body: request });
+export const submitQuizAnswers = (
+  contentId: string,
+  request: QuizSubmitRequest,
+) => {
+  return externalApi.post(`/contents/${contentId}/quiz/submit`, {
+    body: request,
+  }) as Promise<ApiResponseWithData<QuizSubmitResponse>>;
 };
