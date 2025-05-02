2112103ad00b05ebc6a239fe9cfc5f93f3631b22
antis <rkdwldms42@naver.com>
2025-05-02 16:06:46 +0900

    publish: 스켈레톤 컴포넌트 생성

--- front/src/components/player/PlayerFetcher.tsx
+++ front/src/components/player/PlayerFetcher.tsx
@@ -9,7 +9,7 @@ interface PlayerFetcherProps {
 
 const PlayerFetcher = ({ playerId, title }: PlayerFetcherProps) => {
   return (
-    <div className='flex max-w-[800px] flex-col items-center gap-10'>
+    <div className='flex w-[800px] flex-col items-center gap-10'>
       <p className='line-clamp-2 text-center text-2xl font-semibold text-gray-900'>
         {title}
       </p>
