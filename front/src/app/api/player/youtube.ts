19c8c27f27ed8d36968e1cd68a1d3bb2265cc517
안정길 <jeonggilahn@gmail.com>
2025-05-04 11:23:49 +0900

    remove : youtube.ts 중복 제거

--- front/src/app/api/player/youtube.ts
+++
@@ -1,5 +0,0 @@
-export const getYouTubeVideoId = (url: string) => {
-  return url.match(
-    /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/,
-  )?.[1];
-};
