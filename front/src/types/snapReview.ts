79a4d6719693f7acd3ea43f6b11cc804b60b4d57
안정길 <jeonggilahn@gmail.com>
2025-05-06 21:54:51 +0900

    refactor : InfinitedScroll 사용

--- front/src/types/snapReview.ts
+++ front/src/types/snapReview.ts
@@ -1,18 +1,13 @@
+import { InfiniteScrollResponse } from './common';
+
 // 스냅리뷰 목록 조회 응답
-export interface SnapReviewListResponse {
-  content: {
-    dailySnapReviews: {
-      [date: string]: SnapReview[];
-    };
-    totalDays: number;
-  }[];
-  p_pageno: number;
-  p_pagesize: number;
-  totalElements: number;
-  totalPages: number;
-  last: boolean;
-  hasNext: boolean;
-}
+export interface SnapReviewListResponse extends InfiniteScrollResponse<{
+  dailySnapReviews: {
+    [date: string]: SnapReview[];
+  };
+  totalDays: number;
+}>{}
+
 
 // 스냅리뷰 정보
 export interface SnapReview {
