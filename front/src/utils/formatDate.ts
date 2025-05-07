5cb86644dee927f14e21d7ce4b475ea2c06a940c
안정길 <jeonggilahn@gmail.com>
2025-05-07 16:04:05 +0900

    feat : 무한스크롤 및 리팩토링링

--- front/src/utils/formatDate.ts
+++ front/src/utils/formatDate.ts
@@ -1,3 +1,11 @@
 export const formatDate = (dateString: string) => {
   return dateString.split(' ')[0];
 };
+
+export const formatDateToKorean = (dateString: string) => {
+  return new Date(formatDate(dateString)).toLocaleDateString('ko-KR', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+};
