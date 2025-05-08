9d27d032c3df849dea4fb9feb60969fb3249e889
안정길 <jeonggilahn@gmail.com>
2025-05-08 17:01:29 +0900

    refactor : content 사용하게 수정

--- front/src/types/recommend.ts
+++ front/src/types/recommend.ts
@@ -1,31 +1,10 @@
 import { InfiniteScrollResponse } from './common';
+import {Content} from  './content'
 
 // 추천 영상 조회 타입
 export interface SnapReviewListResponse extends InfiniteScrollResponse<Content> {
   originTags?: string;
 }
-
-// 추천 영상 컨텐츠 타입
-export interface Content {
-  contentSeq: number;
-  title: string;
-  description: string;
-  contentType: string;
-  videoId: string;
-  url: string;
-  thumbnailUrl: string;
-  duration: number;
-  tags: string;
-  processStatus: string;
-  analysisData: null;
-  transcript: null;
-  indate: string;
-  completedAt: string;
-  deleteYn: string;
-  processStatusText: string;
-  formattedDuration: string;
-}
-
 
 // 추천 강의 Props 타입
 export interface VideoProps {
