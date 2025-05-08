2e670d9f6897a8de81d99e52f0f7fb5f2a70d6cf
안정길 <jeonggilahn@gmail.com>
2025-05-08 13:32:51 +0900

    feat : Toast 공용 컴포넌트 생성

--- front/src/components/snapbook/Share.tsx
+++ front/src/components/snapbook/Share.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import { toast } from 'sonner';
+import BasicToaster from '@/components/common/BasicToaster';
 import { Button } from '../ui/button';
 
 interface ShareProps {
@@ -14,41 +14,38 @@ const Share = ({ isOpen, onClose, url }: ShareProps) => {
   if (!isOpen) return null;
 
   return (
-    <div className='fixed inset-0 z-50 flex items-center justify-center bg-black/50'>
-      <div className='relative w-full max-w-md rounded-md bg-white p-6 shadow-lg'>
-        <button
-          onClick={onClose}
-          className='absolute top-4 right-4 text-gray-500 hover:text-gray-700'
-        >
-          ✕
-        </button>
-        <div className='mb-6 text-center'>
-          <h2 className='text-lg font-medium text-gray-800'>공유하기</h2>
-        </div>
-        <div className='space-y-6 p-4'>
-          <p className='text-center text-lg text-black'>
-            링크를 갖고 있는 모든 사용자가 방문할 수 있습니다.
-          </p>
-          <div className='flex space-x-2'>
-            <input
-              readOnly
-              value={url}
-              className='flex-1 rounded border border-blue-100 px-3 py-2'
-            />
-            <Button
-              variant='default'
-              className='cursor-pointer'
-              onClick={() => {
-                navigator.clipboard.writeText(url);
-                toast.success('링크가 복사됐습니다.');
-              }}
-            >
-              링크 복사
-            </Button>
+    <>
+      <BasicToaster />
+
+      <div className='fixed inset-0 z-50 flex items-center justify-center bg-black/50'>
+        <div className='relative w-full max-w-md rounded-md bg-white p-6 shadow-lg'>
+          <button
+            onClick={onClose}
+            className='absolute top-4 right-4 text-gray-500 hover:text-gray-700'
+          >
+            ✕
+          </button>
+          <div className='mb-6 text-center'>
+            <h2 className='text-lg font-medium text-gray-800'>공유하기</h2>
+          </div>
+          <div className='space-y-6 p-4'>
+            <p className='text-center text-lg text-black'>
+              링크를 갖고 있는 모든 사용자가 방문할 수 있습니다.
+            </p>
+            <div className='flex space-x-2'>
+              <input
+                readOnly
+                value={url}
+                className='w-full rounded-md border border-gray-300 px-3 py-2'
+              />
+              <Button onClick={() => BasicToaster.default ('링크가 복사되었습니다')}>
+                복사
+              </Button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
