67341c645081b4429d359244cd10e9977cb7548e
안정길 <jeonggilahn@gmail.com>
2025-05-09 08:49:16 +0900

    design : admin page 꾸미기 시작작

--- front/src/app/admin/learninginsights/page.tsx
+++ front/src/app/admin/learninginsights/page.tsx
@@ -1,6 +1,15 @@
-const LearnigInsightPage = () => {
+'use client';
 
-    return(<div>1</div>);
+
+import DateRangeSelector from '@/components/admin/DateRangeSelector';
+
+const LearnigInsightPage = () => {
+    const handleDateChange = ({ startDate, endDate }: { startDate: Date; endDate: Date }) => {
+        // 추후 여기서 데이터 fetching 등을 수행
+      };
+
+    return(     <div className='flex flex-col gap-6 p-6'>
+ <DateRangeSelector onChange={handleDateChange} /></div>);
     
 };
 
--- front/src/app/admin/strategyboard/page.tsx
+++ front/src/app/admin/strategyboard/page.tsx
@@ -1,6 +1,13 @@
-const StrategyBoardPage = () => {
+'use client';
 
-    return(<div>2</div>);
+import DateRangeSelector from '@/components/admin/DateRangeSelector';
+
+const StrategyBoardPage = () => {
+    const handleDateChange = ({ startDate, endDate }: { startDate: Date; endDate: Date }) => {
+        // 추후 여기서 데이터 fetching 등을 수행
+      };
+
+    return(    <div className='flex flex-col gap-6 p-6'> <DateRangeSelector onChange={handleDateChange} /></div>);
     
 };
 
