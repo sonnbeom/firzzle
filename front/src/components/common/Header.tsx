e12dca159a7bb6417070f615698d2e8eb390150d
강지은 <rkdwldms42@naver.com>
2025-05-08 17:48:10 +0900

    refactor: Header 및 HeaderButton 리팩토링

--- front/src/components/common/Header.tsx
+++ front/src/components/common/Header.tsx
@@ -1,25 +1,14 @@
-'use client';
-
+import { Link } from 'lucide-react';
 import Image from 'next/image';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { useState } from 'react';
-import { Button } from '../ui/button';
+import HeaderButton from './HeaderButton';
 
 const Header = () => {
-  const pathname = usePathname();
-  const [isLogin, setIsLogin] = useState(true);
-  const isAdminPage = pathname?.includes('/admin');
-
-  if (pathname?.startsWith('/share')) {
-    return null;
-  }
-
-  return isAdminPage ? (
-    <></>
-  ) : (
+  return (
     <div className='flex w-full items-center justify-between border-b border-gray-300 px-6 py-3 md:py-4 lg:py-6'>
-      <div className='relative h-full w-[60px] md:w-[80px] lg:w-[100px]'>
+      <Link
+        href='/'
+        className='relative h-full w-[60px] md:w-[80px] lg:w-[100px]'
+      >
         <Image
           src='/assets/images/Firzzle.png'
           alt='logo'
@@ -28,17 +17,8 @@ const Header = () => {
           priority
           className='object-contain'
         />
-      </div>
-      {!isLogin ? (
-        <Button variant='default'>시작하기</Button>
-      ) : (
-        <Link
-          href='/mylearning/snapbook'
-          className='bg-white font-medium text-gray-900 hover:bg-gray-50 md:text-lg lg:text-xl'
-        >
-          학습 내역
-        </Link>
-      )}
+      </Link>
+      <HeaderButton />
     </div>
   );
 };
