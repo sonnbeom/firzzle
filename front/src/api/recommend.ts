cd309b1da9ed727b5731619d15dab6663a03805e
안정길 <jeonggilahn@gmail.com>
2025-05-08 14:14:32 +0900

    feat : API 연결용 타입, api 생성성

--- front/src/api/recommend.ts
+++ front/src/api/recommend.ts
@@ -1,9 +1,26 @@
-import { RecommendLecture, ExpertRecommend } from '@/types/recommend';
+import { SnapReviewListResponse, ExpertRecommend } from '@/types/recommend';
 import { externalApi } from './common/apiInstance';
 
 // 추천 강의 조회
-export const getRecommendations = (contentId: string) => {
-  return externalApi.get<RecommendLecture[]>(`/contents/${contentId}/recommendations`);
+export const getRecommendations = async (contentSeq: number,  p_pageno?: number,
+  p_pagesize?: number,
+  p_order?: string,
+  p_sortorder?: string,
+  keyword?: string,
+  category?: string,
+status?: string,
+) => {
+  const {data} = await externalApi.get<SnapReviewListResponse>(`/learning/contents/${contentSeq}/recommendations`,{    
+    params: {
+    p_pageno,
+    p_pagesize,
+    p_order,
+    p_sortorder,
+    keyword,
+    category,
+    status,
+  },});
+  return data;
 };
 
 // 전문가 추천 조회
--- front/src/types/recommend.ts
+++ front/src/types/recommend.ts
@@ -1,9 +1,42 @@
-// 추천 강의 데이터 타입
-export interface RecommendLecture {
+import { InfiniteScrollResponse } from './common';
+
+// 추천 영상 조회 타입
+export type SnapReviewListResponse = InfiniteScrollResponse<{
+  content: Content[];
+  originTags: string;
+}>
+// 추천 영상 컨텐츠 타입
+export interface Content {
+  contentSeq: number;
   title: string;
-  thumbnail: string;
+  description: string;
+  contentType: string;
+  videoId: string;
   url: string;
-  keyword: string;
+  thumbnailUrl: string;
+  duration: number;
+  tags: string;
+  processStatus: string;
+  analysisData: null;
+  transcript: null;
+  indate: string;
+  completedAt: string;
+  deleteYn: string;
+  processStatusText: string;
+  formattedDuration: string;
+}
+
+
+// 추천 강의 활용 데이터 타입
+export interface RecommendVideo {
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
+// 추천 강의 Props 타입
+export interface VideoProps {
+  videos: RecommendVideo[];
+  originTags : string;
 }
 
 // 전문가 추천 데이터 타입
