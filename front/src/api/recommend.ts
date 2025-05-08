57297aba62ea940634640a3bb7f4031504f0e17b
강지은 <rkdwldms42@naver.com>
2025-05-08 18:18:40 +0900

    refactor: InfiniteScrollRequest 타입 적용

--- front/src/api/recommend.ts
+++ front/src/api/recommend.ts
@@ -1,29 +1,24 @@
+import { InfiniteScrollRequest } from '@/types/common';
 import { SnapReviewListResponse, ExpertRecommend } from '@/types/recommend';
 import { externalApi } from './common/apiInstance';
 
 // 추천 강의 조회
-export const getRecommendations = async (contentSeq: number,  p_pageno?: number,
-  p_pagesize?: number,
-  p_order?: string,
-  p_sortorder?: string,
-  keyword?: string,
-  category?: string,
-status?: string,
+export const getRecommendations = async (
+  contentSeq: number,
+  request: InfiniteScrollRequest,
 ) => {
-  const {data} = await externalApi.get<SnapReviewListResponse>(`/learning/contents/${contentSeq}/recommendations`,{    
-    params: {
-    p_pageno,
-    p_pagesize,
-    p_order,
-    p_sortorder,
-    keyword,
-    category,
-    status,
-  },});
+  const { data } = await externalApi.get<SnapReviewListResponse>(
+    `/learning/contents/${contentSeq}/recommendations`,
+    {
+      params: request,
+    },
+  );
   return data;
 };
 
 // 전문가 추천 조회
 export const getExpertRecommendations = (contentId: string) => {
-  return externalApi.get<ExpertRecommend[]>(`/contents/${contentId}/expert-recommendations`);
+  return externalApi.get<ExpertRecommend[]>(
+    `/contents/${contentId}/expert-recommendations`,
+  );
 };
