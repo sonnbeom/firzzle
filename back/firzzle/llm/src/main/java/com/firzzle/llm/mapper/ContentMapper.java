fa525c64c736a19d48a627da9e4404d591fb84ff
장보광 <wkdqhrhkd12@gmail.com>
2025-05-09 10:20:21 +0900

    test : context loads 비활성화

--- back/firzzle/llm/src/main/java/com/firzzle/llm/mapper/ContentMapper.java
+++ back/firzzle/llm/src/main/java/com/firzzle/llm/mapper/ContentMapper.java
@@ -1,11 +1,7 @@
  package com.firzzle.llm.mapper;
 
  import org.apache.ibatis.annotations.Mapper;
- import org.apache.ibatis.annotations.Param;
- import java.util.List;
 
  @Mapper
  public interface ContentMapper {
-
-    
  }
