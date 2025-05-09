7a65924b49871764b7b328f26d12948d446c8034
강지은 <rkdwldms42@naver.com>
2025-05-09 09:19:16 +0900

    fix: 빌드 오류 해결

--- front/src/app/mylearning/contents/page.tsx
+++ front/src/app/mylearning/contents/page.tsx
@@ -1,15 +1,14 @@
-import SearchBar from '@/components/common/SearchBar';
 import MyContentContainer from '@/components/myContents/MyContentContainer';
 
 const MyContentsPage = async () => {
   return (
     <div className='flex flex-col items-center gap-12'>
-      <SearchBar
+      {/* <SearchBar
         value=''
         placeholder='검색어를 입력해주세요.'
         onChange={() => {}}
         onSubmit={() => {}}
-      />
+      /> */}
       <MyContentContainer />
     </div>
   );
