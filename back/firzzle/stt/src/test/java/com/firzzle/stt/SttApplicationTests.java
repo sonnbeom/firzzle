9f10048b2f258e0e4414ad8e02de9097236bfa77
sonnbeom <dhsmf4245@gmail.com>
2025-05-09 11:21:23 +0900

    feat: add: logging module

--- back/firzzle/stt/src/test/java/com/firzzle/stt/SttApplicationTests.java
+++ back/firzzle/stt/src/test/java/com/firzzle/stt/SttApplicationTests.java
@@ -1,8 +1,10 @@
 package com.firzzle.stt;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class SttApplicationTests {
 
