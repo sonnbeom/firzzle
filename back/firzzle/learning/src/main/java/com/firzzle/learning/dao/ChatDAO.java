151a20c9145dcd0739f294ea09a6917a33a90a53
문종하 <3477qq@naver.com>
2025-05-11 11:39:43 +0900

    feat: Learning Chat 학습/시험 모드 및 무한 스크롤 구현
    1. 학습 모드 기능 개발
    2. 시험 모드 기능 구현
    3. prompt에 컨텍스트 추가

--- back/firzzle/learning/src/main/java/com/firzzle/learning/dao/ChatDAO.java
+++ back/firzzle/learning/src/main/java/com/firzzle/learning/dao/ChatDAO.java
@@ -21,6 +21,17 @@ public class ChatDAO extends MyBatisSupport {
     private static final Logger logger = LoggerFactory.getLogger(ChatDAO.class);
     private static final String NAMESPACE = "ChatMapper";
 
+    /**
+     * 채팅 내역 조회 (커서 기반 페이징)
+     */
+    public List<DataBox> selectChatHistoryWithCursor(RequestBox box) {
+        logger.debug("채팅 내역 조회 DAO (커서 기반) - 사용자: {}, 콘텐츠: {}, 커서: {}, 정렬: {} {}, 크기: {}",
+                box.getString("uuid"), box.getLong("contentSeq"), box.getLong("cursor"),
+                box.getString("orderBy"), box.getString("direction"), box.getInt("size"));
+
+        return sqlSession.selectDataBoxList(NAMESPACE + ".selectChatHistoryWithCursor", box);
+    }
+
     public Long insertChat(RequestBox box) {
         logger.debug("채팅 저장 DAO - 사용자: {}, 콘텐츠: {}", box.get("uuid"), box.get("contentSeq"));
         sqlSession.insert(NAMESPACE + ".insertChat", box);
@@ -35,7 +46,7 @@ public class ChatDAO extends MyBatisSupport {
     public List<DataBox> selectRecentChats(RequestBox box) {
         logger.debug("최근 채팅 내역 조회 - 사용자: {}, 콘텐츠: {}, 개수: {}",
                 box.getString("uuid"), box.getLong("contentSeq"), box.getInt("limit"));
-        return sqlSession.selectList(NAMESPACE + ".selectRecentChats", box);
+        return sqlSession.selectDataBoxList(NAMESPACE + ".selectRecentChats", box);
     }
 
 }
