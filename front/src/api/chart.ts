04ff6a1530b1c096540546518b81febf097a072e
안정길 <jeonggilahn@gmail.com>
2025-05-13 13:48:03 +0900

    add : api 추가

--- front/src/api/chart.ts
+++ front/src/api/chart.ts
@@ -34,3 +34,34 @@ export const getEducateChangeRate = async (
   );
   return data;
 };
+
+// 방문자 대비 로그인 전환률 조회
+export const getLoginUserRate = async (startDate: string, endDate: string) => {
+  const { data } = await api.get<TransitionsResponse>(
+    `/admin/strategy/login-rate`,
+    {
+      params: {
+        startDate,
+        endDate,
+      },
+    },
+  );
+  return data;
+};
+
+// 요약 단계별 요약 선호 비율율
+export const getSummaryLevelRate = async (
+  startDate: string,
+  endDate: string,
+) => {
+  const { data } = await api.get<TransitionsResponse>(
+    `/admin/learning/summary-level`,
+    {
+      params: {
+        startDate,
+        endDate,
+      },
+    },
+  );
+  return data;
+};
