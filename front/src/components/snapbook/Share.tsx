f65eb9d0dd5c2bdb90b732a6e7769092b2c6959d
안정길 <jeonggilahn@gmail.com>
2025-05-13 09:16:10 +0900

    fix : Share 링크복사 안되던 것 수정정

--- front/src/components/snapbook/Share.tsx
+++ front/src/components/snapbook/Share.tsx
@@ -38,7 +38,17 @@ const Share = ({ isOpen, onClose, url }: ShareProps) => {
                 value={url}
                 className='w-full rounded-md border border-gray-300 px-3 py-2'
               />
-              <Button onClick={() => BasicToaster.default ('링크가 복사되었습니다')}>
+              <Button
+                onClick={() => {
+                  if (url) {
+                    navigator.clipboard
+                      .writeText(url)
+                      .then(() =>
+                        BasicToaster.default('링크가 복사되었습니다'),
+                      );
+                  }
+                }}
+              >
                 복사
               </Button>
             </div>
