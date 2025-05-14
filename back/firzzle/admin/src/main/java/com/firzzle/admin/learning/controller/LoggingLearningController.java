70d384b0f70c0535bc782b9fbb5e4b502d30e72e
손승범 <no1beom@naver.com>
2025-05-14 11:10:04 +0900

    fix: xml to json

--- back/firzzle/admin/src/main/java/com/firzzle/admin/learning/controller/LoggingLearningController.java
+++ back/firzzle/admin/src/main/java/com/firzzle/admin/learning/controller/LoggingLearningController.java
@@ -5,6 +5,7 @@ import com.firzzle.admin.common.dto.response.ResponseUserLogTransitionDto;
 import com.firzzle.common.response.Response;
 import com.firzzle.common.response.Status;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,7 @@ public class LoggingLearningController {
 
     private final LoggingLearningService loggingLearningService;
 
-    @GetMapping("/summary-level")
+    @GetMapping(value = "/summary-level", produces = MediaType.APPLICATION_JSON_VALUE)
     public Object getSummaryPreferenceRate(@RequestParam LocalDate startDate, @RequestParam LocalDate endDate) {
         List<ResponseUserLogTransitionDto> responseDto = loggingLearningService.getSummaryPreferenceRate(startDate, endDate);
         Response<List<ResponseUserLogTransitionDto>> response = Response.<List<ResponseUserLogTransitionDto>>builder()
@@ -30,7 +31,7 @@ public class LoggingLearningController {
                 .build();
         return ResponseEntity.ok(response);
     }
-    @GetMapping("/snap-review")
+    @GetMapping(value = "/snap-review", produces = MediaType.APPLICATION_JSON_VALUE)
     public Object getSnapReviewWriteRate(@RequestParam LocalDate startDate, @RequestParam LocalDate endDate) {
         List<ResponseUserLogTransitionDto> responseDto = loggingLearningService.getSnapReviewWriteRate(startDate, endDate);
         Response<List<ResponseUserLogTransitionDto>> response = Response.<List<ResponseUserLogTransitionDto>>builder()
