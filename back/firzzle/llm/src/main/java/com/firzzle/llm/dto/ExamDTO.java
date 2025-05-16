2aadc6c49ca60d07951d52b922637fe7d9d044d0
장보광 <wkdqhrhkd12@gmail.com>
2025-05-16 09:57:51 +0900

    fix: 오류 수정

--- back/firzzle/llm/src/main/java/com/firzzle/llm/dto/ExamDTO.java
+++ back/firzzle/llm/src/main/java/com/firzzle/llm/dto/ExamDTO.java
@@ -1,32 +1,36 @@
 package com.firzzle.llm.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 /**
- * @Class Name : ContentDTO.java
- * @Description : 콘텐츠 정보 DTO (MyBatis 매핑 및 API 응답용 DTO)
+ * @Class Name : AiExamDTO.java
+ * @Description : AI 시험 문제 DTO (MyBatis 매핑 및 API 응답용 DTO)
  * @author Firzzle
- * @since 2025. 5. 15.
+ * @since 2025. 5. 16.
  */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Schema(description = "시험모드 문제 정보")
+@Schema(description = "AI 시험 문제 정보")
 public class ExamDTO {
-    @Schema(description = "시험 문제 일련 번호", example = "1001")
-    private Long ExamSeq;
 
-    @Schema(description = "유튜브 영상 일련 번호", example = "1001")
-    private Long ContentSeq;
+    @Schema(description = "시험 일련 번호", example = "101")
+    private Integer examSeq;
 
-    @Schema(description = "질문 내용", example = "도메인 주도 설계(DDD)란 무엇인가요?")
-    private String QuestionContent;
+    @Schema(description = "콘텐츠 일련 번호", example = "1001")
+    private Integer contentSeq;
 
-    @Schema(description = "모범 답인", example = "DDD란 무엇입니다.")
-    private String ModelAnswer;
+    @Schema(description = "문제 내용", example = "다음 중 DDD의 핵심 개념이 아닌 것은?")
+    private String questionContent;
+
+    @Schema(description = "모델 답안", example = "C. 이벤트 스토밍")
+    private String modelAnswer;
+
+    @Schema(description = "영상 해당 부분 시간", example = "00:05:12")
+    private String startTime;
+
+    @Schema(description = "참고 내용", example = "이 내용은 DDD의 배경 설명에서 다뤄집니다.")
+    private String referenceText;
 }
