1736480d3e67fbed2a6db4be0104edec979ea783
강지은 <rkdwldms42@naver.com>
2025-05-19 09:14:12 +0900

    feat: content 경로의 loading 페이지 생성

--- front/src/app/content/[id]/loading.tsx
+++ front/src/app/content/[id]/loading.tsx
@@ -1,17 +1,7 @@
 import BasicSkeleton from '@/components/common/BasicSkeleton';
 
 const Loading = () => {
-  return (
-    <div className='flex h-full w-full gap-5'>
-      <div className='hidden flex-2 flex-col gap-6 lg:flex xl:flex-3'>
-        <BasicSkeleton className='aspect-video w-full' />
-        <BasicSkeleton className='flex h-full flex-col gap-2' />
-      </div>
-      <div className='flex flex-3 flex-col items-center gap-2 lg:gap-4 xl:flex-7'>
-        <BasicSkeleton className='h-full w-full' />
-      </div>
-    </div>
-  );
+  return <BasicSkeleton className='h-full w-full' />;
 };
 
 export default Loading;
