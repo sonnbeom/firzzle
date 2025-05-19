d6c1a80ec439245e0a6a1a625f3334568df67c96
강지은 <rkdwldms42@naver.com>
2025-05-19 15:41:38 +0900

    fix: 쿠키 설정 디버깅

--- front/src/services/authServices.ts
+++ front/src/services/authServices.ts
@@ -2,7 +2,6 @@ import { setCookie } from '@/actions/auth';
 
 export const login = async (accessToken: string) => {
   if (accessToken) {
-    console.log('로그인 서비스', accessToken);
     window.history.replaceState({}, document.title, window.location.pathname);
     setCookie('accessToken', accessToken);
     return '/content';
