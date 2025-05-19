5685e25c306eb19647c99c6d5a35914944c4392c
강지은 <rkdwldms42@naver.com>
2025-05-19 21:54:20 +0900

    fix: 헤더의 accessToken 유무 로직 수정

--- front/src/components/common/Header.tsx
+++ front/src/components/common/Header.tsx
@@ -1,18 +1,10 @@
-'use client';
-
 import Image from 'next/image';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { getCookie } from '@/actions/auth';
 import HeaderButton from './HeaderButton';
 
-const Header = () => {
-  const pathname = usePathname();
-
-  // share 페이지에서는 Header를 숨김
-  if (pathname?.startsWith('/share/')) {
-    return null;
-  }
-
+const Header = async () => {
+  const accessToken = await getCookie('accessToken');
   return (
     <div className='sticky top-0 z-50 flex w-full items-center justify-between border-b border-gray-300 bg-white px-6 py-4 lg:py-5'>
       <Link
@@ -28,7 +20,7 @@ const Header = () => {
           className='object-contain'
         />
       </Link>
-      <HeaderButton />
+      <HeaderButton accessToken={accessToken} />
     </div>
   );
 };
