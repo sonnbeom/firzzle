ab97abb089c666f47ecb42ed4535a9873e0f3798
강지은 <rkdwldms42@naver.com>
2025-05-19 15:34:17 +0900

    fix: 로그인 디버깅

--- front/src/services/authServices.ts
+++ front/src/services/authServices.ts
@@ -2,6 +2,7 @@ import { setCookie } from '@/actions/auth';
 
 export const login = async (accessToken: string) => {
   if (accessToken) {
+    console.log('로그인 서비스', accessToken);
     window.history.replaceState({}, document.title, window.location.pathname);
     setCookie('accessToken', accessToken);
     return '/content';
